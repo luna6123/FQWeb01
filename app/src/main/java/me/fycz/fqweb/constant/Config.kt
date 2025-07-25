b23a6e388c53ff39093b267e650bdac28d9065a3
luna6123 <95160348+luna6123@users.noreply.github.com>
2025-07-26 02:59:08 +0800

    Update Config.kt

--- app/src/main/java/me/fycz/fqweb/constant/Config.kt
+++ app/src/main/java/me/fycz/fqweb/constant/Config.kt
@@ -12,10 +12,10 @@ import me.fycz.fqweb.utils.findClass
 object Config {
 
     const val TRAVERSAL_CONFIG_URL =
-        "https://gitee.com/fengyuecanzhu/FQWeb/raw/frpc/traversal/config.json"
+        "https://gitee.com/sunianOvO/FQWeb/blob/patch-1/traversal/config.json"
 
     const val DEFAULT_USER_AGENT =
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4168.3 Safari/537.36"
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
 
     private val dragonClassloader by lazy { GlobalApp.getClassloader() }
 

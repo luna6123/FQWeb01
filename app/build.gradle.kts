ac6d35c5dff492d9f446e1b3803bf3edbd891e96
luna6123 <95160348+luna6123@users.noreply.github.com>
2025-08-15 16:41:20 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,9 +87,6 @@ dependencies {
     //frpc
     implementation(files("libs/frpclib.aar"))
 
-    //config
-    implementation "androidx.recyclerview:recyclerview:1.3.2"
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
